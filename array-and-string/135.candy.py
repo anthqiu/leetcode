45eba265d9b5e143bbcb93d85d5341dc3e0ac707
Qiu Shi <i@qiushi.xyz>
2024-02-14 22:59:31 +0800

    1st solution to problem 135

--- array-and-string/135.candy.py
+++ array-and-string/135.candy.py
@@ -9,70 +9,71 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         if len(ratings) == 1:
             return 1
-        # find each increase/decrease sequence
-        current_level = 1
-        min_level = 1
-        current_segment_length = 1
-        current_segment_candies = 1
-        num_candies = 0
-        current_direction = ratings[1] - ratings[0]
-        print(current_level, " init")
-        for idx in range(1, len(ratings)):
-            delta = ratings[idx] - ratings[idx - 1]
-            if current_direction > 0 and delta > 0:
-                current_segment_length += 1
-                current_level += 1
-                current_segment_candies += current_level
-                print(current_level, " +")
-            elif current_direction < 0 and delta < 0:
-                current_segment_length += 1
-                current_level -= 1
-                min_level = min(min_level, current_level)
-                current_segment_candies += current_level
-                print(current_level, " <")
-            elif current_direction == 0 and delta == 0:
-                if idx < len(ratings) - 1 and ratings[idx + 1] != ratings[idx]:
-                    additional_candies_per_child = 1 - min_level
-                    additional_candies = additional_candies_per_child * current_segment_length
-                    current_segment_candies += additional_candies
-                    num_candies += current_segment_candies
-                    current_direction = ratings[idx + 1] - ratings[idx]
-                    current_segment_length = 1
-                    current_segment_candies = 1
-                    current_level = 1
-                    min_level = 1
-                    print(current_level, " *0 ", num_candies)
-                else:
-                    current_segment_length += 1
-                    current_segment_candies += current_level
-                    print(current_level, " =")
+        candies = self.init_candies(ratings)
+        n = len(ratings)
+        for i in range(n):
+            self.optimize_candies(ratings, candies, i, n, False)
+        for i in range(n):
+            self.optimize_candies(ratings, candies, n - 1 - i, n, True)
+        cnt = 0
+        for i in candies:
+            cnt += i
+        return cnt
+
+    def init_candies(self, ratings: List[int]) -> List[int]:
+        candies = [0 for i in range(len(ratings))]
+        current_candies = 1
+        candies[0] = 1
+        min_candies = 1
+
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                current_candies += 1
+                candies[i] = current_candies
             else:
-                additional_candies_per_child = 1 - min_level
-                additional_candies = additional_candies_per_child * current_segment_length
-                current_segment_candies += additional_candies
-                num_candies += current_segment_candies
-                if delta == 0 and idx < len(ratings) - 1 and ratings[idx + 1] != ratings[idx]:
-                    delta = ratings[idx + 1] - ratings[idx]
-                current_direction = delta
-                current_segment_length = 1
-                current_level = 1 if delta <= 0 else 2
-                min_level = current_level
-                current_segment_candies = current_level
-                print(current_level, " * ", num_candies)
-        additional_candies_per_child = max(0, 1 - min_level)
-        additional_candies = additional_candies_per_child * current_segment_length
-        current_segment_candies += additional_candies
-        num_candies += current_segment_candies
-        print(num_candies)
-        return num_candies
+                current_candies -= 1
+                candies[i] = current_candies
+                min_candies = min(current_candies, min_candies)
+
+        increase = 1 - min_candies
+
+        for i in range(len(ratings)):
+            candies[i] += increase
+
+        return candies
+
+    def optimize_candies(self, ratings: List[int], candies: List[int], idx: int, n: int, right: bool) -> None:
+        has_left = idx != 0
+        has_right = idx != n - 1
+        left_larger = True if not has_left else ratings[idx - 1] >= ratings[idx]
+        right_larger = True if not has_right else ratings[idx + 1] >= ratings[idx]
+        if left_larger and right_larger:
+            candies[idx] = 1
+            return
+        left_min = -1
+        if has_left:
+            if left_larger:
+                left_min = candies[idx - 1] - 1  # 0 if left_equal else 1
+            else:
+                left_min = candies[idx - 1] + 1
+        right_min = -1
+        if has_right:
+            if right_larger:
+                right_min = candies[idx + 1] - 1  # if right_equal else 1
+            else:
+                right_min = candies[idx + 1] + 1
+        if left_larger:
+            candies[idx] = right_min
+        elif right_larger:
+            candies[idx] = left_min
+        else:
+            candies[idx] = max(left_min, right_min)
 
 
 if __name__ == '__main__':
-    ratings = [1, 3, 4, 5, 2]
-    print(11 == Solution().candy(ratings))
-    ratings = [1, 2, 2, 2, 1]
-    print(7 == Solution().candy(ratings))
-    ratings = [1, 0, 2]
-    print(5 == Solution().candy(ratings))
-    ratings = [1, 3, 2, 2, 1]
-    print(7 == Solution().candy(ratings))
+    print(11 == Solution().candy([1, 3, 4, 5, 2]))
+    print(7 == Solution().candy([1, 2, 2, 2, 1]))
+    print(5 == Solution().candy([1, 0, 2]))
+    print(7 == Solution().candy([1, 3, 2, 2, 1]))
+    print(9 == Solution().candy([1, 2, 4, 4, 3]))
+    print(13 == Solution().candy([1, 2, 87, 87, 87, 2, 1]))
