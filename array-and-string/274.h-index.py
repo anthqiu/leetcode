16154dbc29dcc4b097b35e3d9118b523244e92e8
anthqiu <i@qiushi.xyz>
2024-02-20 21:45:40 +0800

    solution 2 to problem 274

--- array-and-string/274.h-index.py
+++ array-and-string/274.h-index.py
@@ -7,9 +7,21 @@ from typing import List
 
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True)
         l = len(citations)
+
+        # count #papers that got #citations. indices are #citations, values are #papers with #citations
+        cit_freq = [0 for i in range(l + 1)]
+        for c in citations:
+            # if paper got #citations more than len(citations), count as len(citations)
+            if c >= l:
+                cit_freq[l] += 1
+            else:
+                cit_freq[c] += 1
+        cnt = 0
         for i in range(l):
-            if citations[l - 1 - i] >= l - i:
+            # (l - i) is #papers, cnt is #papers with #citations greater or equal than (l - i)
+            cnt += cit_freq[l - i]
+            if cnt >= l - i:
+                # if #papers with [#citations greater than (l - i)] is greater than or equal to (l - i), then return
                 return l - i
         return 0
